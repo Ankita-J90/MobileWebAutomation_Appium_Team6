94a87ab27a481be4aecc3d517af693a6f822b635
Ankita-J90 <ankita_jadhav@persistent.com>
2021-09-20 14:59:05 +0530

    added test case to verify logout

--- src/test/java/com/tests/Tc_Logout.java
+++ src/test/java/com/tests/Tc_Logout.java
@@ -27,13 +27,11 @@ public class Tc_Logout {
 		driver.findElement(By.xpath("/html/body/div[1]/div/div/form/div[3]/div/button")).submit();
 		Thread.sleep(2000);
 		js.executeScript("window.scrollBy(350,0)", "");
-		driver.findElement(By.xpath("//*[@id=\"nav-toggle\"]/i")).click();
-	 
+		driver.findElement(By.xpath("//*[@id=\"nav-toggle\"]/i")).click(); 
 		Thread.sleep(2000);
 	    driver.findElement(By.xpath("//*[@id=\"nav-menu\"]/ul/li[3]/a")).click();
 	    Thread.sleep(2000);
 	    String s=driver.findElement(By.xpath("/html/body/div[1]/div/div/form/span")).getText();
-	
         Assert.assertEquals("Welcome",s);
 
   }
